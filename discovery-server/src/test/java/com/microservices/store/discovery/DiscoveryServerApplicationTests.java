e29c76ea52b5812796d535bd744a0f6e32253918
ccahui <kris_ti4n@hotmail.com>
2022-11-10 16:54:38 -0500

    Discovery Server: Eureka y configuracion de Docker

---
+++ discovery-server/src/test/java/com/microservices/store/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.store.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- product-service/src/main/java/com/microservices/store/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservices/store/product/ProductServiceApplication.java
@@ -4,12 +4,14 @@ import com.microservices.store.product.controller.CategoryController;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
