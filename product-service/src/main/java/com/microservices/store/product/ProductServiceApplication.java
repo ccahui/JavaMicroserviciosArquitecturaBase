e78ec1fb85bba1ae7d9a869898b9d04dc6729df4
ccahui <kris_ti4n@hotmail.com>
2022-11-12 16:18:08 -0500

    Api Gateway: Shopping, Product, Customer  y configuracione para su ejecucion en Docker

--- product-service/src/main/java/com/microservices/store/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservices/store/product/ProductServiceApplication.java
@@ -4,6 +4,7 @@ import com.microservices.store.product.controller.CategoryController;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
