ca297319c246e5f46e6a33f71a83072324da8754
ccahui <kris_ti4n@hotmail.com>
2022-11-25 12:22:33 -0500

    Config Server: Debug

--- product-service/src/main/java/com/microservices/store/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservices/store/product/ProductServiceApplication.java
@@ -2,11 +2,13 @@ package com.microservices.store.product;
 
 import com.microservices.store.product.controller.CategoryController;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@RestController
 public class ProductServiceApplication {
-
+	@Value("${data.perfil}")
+	private String value;
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
@@ -26,4 +30,8 @@ public class ProductServiceApplication {
 		return new ModelMapper();
 	}
 
+	@GetMapping("/prueba")
+	public String prueba(){
+		return "Pefil "+value;
+	}
 }
