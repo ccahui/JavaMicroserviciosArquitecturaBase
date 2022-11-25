eba152429a9d488ebaa768d7a325aa5efe77dd0c
ccahui <kris_ti4n@hotmail.com>
2022-11-25 14:50:41 -0500

    Config Server: Customer Service Ok

--- shopping-service/src/main/java/com/microservices/store/shopping/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/com/microservices/store/shopping/ShoppingServiceApplication.java
@@ -1,19 +1,27 @@
 package com.microservices.store.shopping;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
+@RestController
 public class ShoppingServiceApplication {
-
+	@Value("${data.perfil}")
+	private String value;
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingServiceApplication.class, args);
 	}
-
+	@GetMapping("/prueba")
+	public String prueba(){
+		return "Pefil "+value;
+	}
 }
