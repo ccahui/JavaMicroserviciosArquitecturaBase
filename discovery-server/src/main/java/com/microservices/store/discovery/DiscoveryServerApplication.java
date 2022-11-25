d87964802a985591a681548b622ee46cd13b0065
ccahui <kris_ti4n@hotmail.com>
2022-11-25 15:24:51 -0500

    Config Server: Debug

--- discovery-server/src/main/java/com/microservices/store/discovery/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/microservices/store/discovery/DiscoveryServerApplication.java
@@ -1,15 +1,26 @@
 package com.microservices.store.discovery;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
 public class DiscoveryServerApplication {
-
+	@Value("${data.perfil}")
+	private String value;
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
-
+	@GetMapping("/prueba")
+	public String prueba(){
+		return "Pefil "+value;
+	}
 }
